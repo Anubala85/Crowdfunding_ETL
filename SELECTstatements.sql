f4480ad6c6d37f58b9396c559dafa5ed87e66dc9
Anubala85 <anushabalasubramaniann@gmail.com>
2024-04-29 20:15:05 -0700

    Final consolidated

---
+++ SELECTstatements.sql
@@ -0,0 +1,16 @@
+-- The script used to display data from each table using a SELECT statement
+-- 1.Campaign Table
+    SELECT * FROM public."Campaign"
+    ORDER BY cf_id ASC 
+
+-- 2.Category table
+    SELECT * FROM public."Campaign"
+    ORDER BY cf_id ASC 
+
+-- 3.SubCategory Table
+    SELECT * FROM public."Subcategory"
+    ORDER BY subcategory_id ASC 
+
+--4.Contacts Table
+    SELECT * FROM public.contacts
+    ORDER BY contact_id ASC 
